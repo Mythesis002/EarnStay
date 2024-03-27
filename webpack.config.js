bd8dcd781e59352144d9f52da7561cf82d88a127
Mythesis002 <131542998+Mythesis002@users.noreply.github.com>
2024-03-27 12:29:44 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const path = require("path");
 module.exports = {
     // Other webpack configurations...
     resolve: {
-        fallback: {
-            "path": require.resolve("path-browserify"),
-            "http": require.resolve("stream-http")
-            // Add more fallbacks if needed
-        }
-    }
+  fallback: {
+    zlib: require.resolve('browserify-zlib'),
+    querystring: require.resolve('querystring-es3'),
+    stream: require.resolve('stream-browserify')
+  }
+}
+
 };
